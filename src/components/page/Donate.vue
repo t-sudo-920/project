2af4df8206dcef98ad88ef1c0146ac61e47c5ff5
zs <mr.87878787@qq.com>
2020-05-05 08:23:13 +0800

    提交代码

---
+++ src/components/page/Donate.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-redpacket_fill"></i> 联系我哦
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">微信号DL2582588</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    baseUrl: './',
+    assetsDir: 'static',
+    productionSourceMap: false
+}
